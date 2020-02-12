55218f931fc1b3eb60c4e05935b59a46023956da
FilippSokolov <Filipp.Sokolov@jetbrains.com>
2020-02-12 13:45:17 +0300

    zzz

--- src/main/java/RetrofitJavaAppExample.java
+++ src/main/java/RetrofitJavaAppExample.java
@@ -13,46 +13,5 @@ import java.io.IOException;
 
 public class RetrofitJavaAppExample {
 
-    interface GithubGistService {
-        @GET("https://gist.githubusercontent.com/FilippSokolov/f5b81b5c07ef5f73bd40c964295c9e9b/raw/9edc125ec113d74bc2144e5640c42eaaa3cb0837/{id}")
-        Call<Task> getGithubGist(@Path("id") String id);
-    }
 
-    @Root(name = "task")
-    public static class Task {
-        @Element(name = "id")
-        private long id;
-        @Element(name = "author")
-        private String author;
-        @Element(name = "title")
-        private String title;
-
-        public Task() {
-        }
-    }
-
-
-    public static void main(String[] args) {
-
-        System.out.println("Test - xml converter");
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://localhost:3000")
-                .addConverterFactory(SimpleXmlConverterFactory.create())
-                .build();
-
-        GithubGistService gistService = retrofit.create(GithubGistService.class);
-
-
-
-        try {
-            Response<Task> response = gistService.getGithubGist("test1.xml").execute();
-            Task task = response.body();
-            System.out.println("Request done");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
 }
